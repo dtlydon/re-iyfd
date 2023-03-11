d5f6f259d340bd5e65a57b5ce9c3261d9da9e54b
Dan Lydon <dtlydon@gmail.com>
2023-03-10 19:00:28 -0600

    More github action fun

--- client/src/components/play/userPlay.tsx
+++ client/src/components/play/userPlay.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
 import { UserChoice } from "../../models/interfaces";
 import choicesService from "../../services/choicesService";
 import { getCurrentRound } from "../../common/utils";
